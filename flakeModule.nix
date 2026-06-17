0c1d9ed98cec70f235f0198ba9a834b618e01c0a
removed-user <caseyriley94@gmail.com>
2026-06-17 15:25:01 -0400

    Update flakeModule.nix

--- flakeModule.nix
+++ flakeModule.nix
@@ -3,5 +3,5 @@ let
   inherit (flake-parts-lib) importApply;
 in
 {
-  flake.flakeModules.default = importApply ./flake-module.nix { localFlake = self; inherit withSystem; };
+  flake.flakeModules.default = importApply ./flake-module.nix { providerFlake = self; inherit withSystem; };
 }
