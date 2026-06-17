682456ff214484a99b3cda2827023aaabc4dffc7
casey <caseyriley94@gmail.com>
2026-06-17 19:23:47 -0400

    added flake.nix and other functionTo file for tests

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      disabledModules = [
+        inputs.flake-parts.flakeModules.nixosModules
+        inputs.flake-parts.flakeModules.nixosConfigurations
+        inputs.flake-parts.flakeModules.apps
+        inputs.flake-parts.flakeModules.devShells
+        inputs.flake-parts.flakeModules.formatter
+      ];
 
-      # 2. Local module evaluation
-      imports = [ ./modules/my-feature.nix ];
+      imports = [
+        inputs.flake-parts.flakeModules.flakeModules
+        inputs.flake-parts.flakeModules.modules
+        inputs.flake-parts.flakeModules.debug
+      ];
 
       # 3. Downstream export
-      flake.flakeModules.default = ./modules/my-feature.nix;
+      flake.flakeModules.default = ./functionTo_flakeModule.nix;
     };
 }
